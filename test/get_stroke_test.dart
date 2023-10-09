91ebec564b8d75262a54914e40d7c5c05b8f2eb9
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-10-09 14:29:35 +0100

    test: add golden test for single-point stroke

--- test/get_stroke_test.dart
+++ test/get_stroke_test.dart
@@ -1,12 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:perfect_freehand/src/get_stroke.dart';
 
-import 'inputs/one_point.dart';
+import '_stroke_drawer.dart';
+import 'example_inputs/one_point.dart';
 
 void main() {
   group('getStroke', () {
-    test('gets stroke from a line with a single point', () {
-      expect(getStroke(onePoint), defaultOutputOnePoint);
+    testWidgets('gets stroke from a line with a single point', (tester) async {
+      final stroke = getStroke(onePoint);
+      
+      await tester.pumpWidget(StrokeDrawer(stroke: stroke));
+
+      await expectLater(
+        find.byType(StrokeDrawer),
+        matchesGoldenFile('example_inputs/one_point.png'),
+      );
     });
   });
 }
