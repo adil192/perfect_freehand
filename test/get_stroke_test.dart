11e58d7524ca8f13c53d182689bc9ad14883bd3c
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-10-09 15:12:41 +0100

    test: add more example inputs

--- test/get_stroke_test.dart
+++ test/get_stroke_test.dart
@@ -1,32 +1,46 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:perfect_freehand/src/get_stroke.dart';
 
 import '_stroke_drawer.dart';
+import 'example_inputs/hey.dart';
+import 'example_inputs/many_points.dart';
+import 'example_inputs/number_pairs.dart';
 import 'example_inputs/one_point.dart';
+import 'example_inputs/two_equal_points.dart';
 import 'example_inputs/two_points.dart';
+import 'example_inputs/with_duplicates.dart';
+
+const _exampleInputs = {
+  "one_point": onePoint,
+  "two_points": twoPoints,
+  "two_equal_points": twoEqualPoints,
+  "with_duplicates": withDuplicates,
+  "number_pairs": numberPairs,
+  "many_points": manyPoints,
+  "hey": hey,
+};
 
 void main() {
   group('getStroke', () {
-    testWidgets('gets stroke from a line with a single point', (tester) async {
-      final stroke = getStroke(onePoint);
-      
-      await tester.pumpWidget(StrokeDrawer(stroke: stroke));
+    for (final entry in _exampleInputs.entries) {
+      testWidgets('gets stroke from ${entry.key}', (tester) async {
+        tester.view.physicalSize = const Size(4000, 2000);
+        addTearDown(tester.view.resetPhysicalSize);
 
-      await expectLater(
-        find.byType(StrokeDrawer),
-        matchesGoldenFile('example_inputs/one_point.png'),
-      );
-    });
+        final stroke = getStroke(entry.value);
 
-    testWidgets('gets stroke from a line with two points', (tester) async {
-      final stroke = getStroke(twoPoints);
-      
-      await tester.pumpWidget(StrokeDrawer(stroke: stroke));
+        expect(stroke.any((offset) {
+          return offset.dx.isNaN || offset.dy.isNaN;
+        }), false, reason: 'stroke contains NaNs');
+        
+        await tester.pumpWidget(StrokeDrawer(stroke: stroke));
 
-      await expectLater(
-        find.byType(StrokeDrawer),
-        matchesGoldenFile('example_inputs/two_points.png'),
-      );
-    });
+        await expectLater(
+          find.byType(StrokeDrawer),
+          matchesGoldenFile('example_inputs/${entry.key}.png'),
+        );
+      });
+    }
   });
 }
